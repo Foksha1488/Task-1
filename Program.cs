2ea28f58771ca9806e70739770d07349533eb56e
Foksha1488 <maksimfoksha97@gmail.com>
2025-02-19 09:21:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {//Заполнить одномерный массив из n элементов с клавиатуры и вывести его на экран.
+        static void Main(string[] args)
+        {
+            //Console.Write(9);
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"{i + 1}:");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Количество элементов:");
+            //foreach (int element in array)
+            //{
+            //    Console.Write(element + " ");
+            //}
+            //Console.ReadKey();
+        }
+    }
+}
+
+       
